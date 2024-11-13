e369f558547d5f17cd7da7a49815e41dbff56748
SebC750 <SebC89999@gmail.com>
2024-11-13 13:41:26 -0500

    Final adjustments + more about section info - Made some final adjustments to the webpage. I rearranged the order of the navbar links to follow the same order as the sections. - Also added new information on my about page to clarify some things about myself and my flexibility for jobs. - Added information to readme.me file to give a brief overview of the website. - Minor SEO optimizations - Added attributions for icons used in the tech stack section in compliance with legal requirements. - Replaced original icons for github and linkedin with icons provided on their websites to comply with their brand guidelines.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Portfolio_Website
-My website that showcases my portfolio of web and desktop applications I have worked on and created, from personal projects to team projects.
+# Portfolio Website
+This is my official website for showcasing my projects, from personal to work related projects. It includes important information such as: 
+1. About section - I introduce myself, my hobbies and what I do as a software developer. I also provide information about my flexibility with regards to what clients will need from me (relocation etc.).
+2. Tech stack section - This section showcases all of the technologies I have worked with which includes frontend languages, backend languages, build tools, cloud services and databases.
+3. Projects section - A comprehensive collection of my projects. Each one includes a description explaining my motivation, contributions and impacts I have made for my team, clients and myself.
+4. Footer - My contact information is included at the end for any inquiries regarding interest in my skills.
+
+I will be updating this website periodically to reflect new information such as completion/improvement of projects, adoption of new tech skills and other future sections.
--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -4,33 +4,40 @@ const Footer = () => {
       <div className="bg-dark p-5 text-yellow" id="contacts">
         <div className="container container-sm-fluid">
           <div className="row">
-            <div className="col">
+            <div className="col-12 col-md-4">
               <h4 className="fw-bold"> Sebastian Chalarca </h4>
-              <ul>
+              <ul className="ps-0">
                 <li> <i className="bi bi-geo-alt-fill"></i> New York City, NY</li>
-                <li> <i className="bi bi-envelope-fill"></i> [Email goes here] </li>
-                <li> <i className="bi bi-telephone-fill"></i> [Number goes here]</li>
+                <li> <i className="bi bi-envelope-fill"></i> sebastian.chalarca.dev@gmail.com</li>
+                <li> <i className="bi bi-telephone-fill"></i> 929-216-4699</li>
               </ul>
             </div>
-            <div className="col">
+            <div className="col-12 col-md-4 d-none d-sm-block">
               <h4 className="fw-bold"> Quick links </h4>
-              <ul>
+              <ul >
                 <li> <a className="nav-link" href="#intro-section"> Home </a> </li>
                 <li> <a className="nav-link" href="#about-section"> About me </a> </li>
                 <li> <a className="nav-link" href="#skills-section"> Tech stack </a> </li>
                 <li> <a className="nav-link" href="#portfolio"> Portfolio </a>  </li>
               </ul>
             </div>
-            <div className="col">
+            <div className="col-12 col-md-4 d-none d-sm-block">
               <h4 className="fw-bold"> Also on </h4>
               <ul className="nav gap-2">
-                <li className="nav-item"> <a className="pb-1" href="https://github.com/SebC750"> <i className="bi bi-github text-yellow"></i> </a> </li>
-                <li className="nav-item"> <a className="pb-1" href="www.linkedin.com/in/sebastian-chalarca-603939201"> <i className="bi bi-linkedin text-yellow"></i> </a> </li>
+                <li > <a className="pb-1" href="https://github.com/SebC750" aria-label="Go to my github profile to see my works"> <img src="LI-In-Bug.png" loading="lazy" aria-label="LinkedIn profile icon link" alt="linkedin logo" height="35" width="40" />  </a> </li>
+                <li > <a className="pb-1" href="www.linkedin.com/in/sebastian-chalarca-603939201" aria-label="Go to my linkedin profile to learn more about me."> <img src="github-mark-white.svg" loading="lazy" aria-label="GitHub profile icon link" alt="github logo" height="35" width="35" /> </a> </li>
               </ul>
             </div>
           </div>
         </div>
-        <p className="footer-copyright-text"> 2024, Sebastian Chalarca All rights reserved. </p>
+        <p className="attribution-text text-center opacity-50 text-yellow"> Icons provided by <a href="https://devicon.dev/">Devicon</a>. All product names, logos, and brands are property of their respective owners.
+          Java and the Java logo are trademarks of Oracle Corporation.
+          PHP is a trademark of the PHP Group.
+          Vercel and the Vercel design are trademarks of Vercel, Inc.
+          Google Cloud and the Google Cloud logo are trademarks of Google LLC.
+          GitHub and the GitHub logo are trademarks of GitHub, Inc.
+          LinkedIn and the LinkedIn logo are trademarks of LinkedIn Corporation. </p>
+        <p className="footer-copyright-text opacity-50"> 2024, Sebastian Chalarca </p>
       </div>
     </>
   )

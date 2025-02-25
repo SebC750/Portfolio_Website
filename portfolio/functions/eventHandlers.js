d426350f955ae241bffc16669f1c48cd35491042
SebC750 <SebC89999@gmail.com>
2025-02-24 23:37:51 -0500

    Finished renovation of portfolio website - Recreated my portfolio website to be more minimalistic and clean, creating a more appealing look for visitors - Rewrote sections to be more concise and to the point, making it easier for employers to understand me. - Rewrote portfolio page project cards to be simpler and better organized.

--- portfolio/functions/eventHandlers.js
+++ portfolio/functions/eventHandlers.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", async function(){
     const techStackData = await response.json()
     const techList = document.getElementById("techList")
     techStackData["technologies"].map((val) =>{
-        techList.innerHTML += `<div class="col-sm-6 col-md-4 col-lg-3">
+        techList.innerHTML += `<div class="col-6 col-md-4 col-lg-3">
            <img src="${val.src}" alt="${val.alt}" height="${LOGO_DIMENSIONS.height}" width="${LOGO_DIMENSIONS.width}"/>
         </div>
         `
